0f244c04fbd173fb901aaa6b4a4ee8a913e93eb4
Gerd Watmann <watmann@de.ibm.com>
2020-09-24 22:01:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,12 +25,12 @@ resource ibm_resource_instance logdna {
 
   name              = "${var.unique_id}-logdna"
   service           = "logdna"
-  plan              = "${var.logging_plan}"
-  location          = "${var.ibm_region}"
-  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
+  plan              = var.logging_plan
+  location          = var.ibm_region
+  resource_group_id = data.ibm_resource_group.resource_group.id
 
   parameters = {
-    service-endpoints = "${var.end_points}"
+    service-endpoints = var.end_points
   }
 
 }
